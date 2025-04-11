961d6aaa2703dbb4e544932001648947a5d79930
Sahara-Islam <saharaevaislam16@gmail.com>
2025-04-11 16:49:46 +0000

    Repository Update - 2025-04-11 16:49:45

--- ExtraCredit/factorial.py
+++ ExtraCredit/factorial.py
@@ -11,4 +11,19 @@
 
 if __name__ == "__main__":
     ...
-    
+
+num = int(input("Enter a positive number: "))
+
+
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1.")
+else:
+    factorial = 1
+    i = 1
+    while i <= num:
+        factorial *= i
+        i += 1
+
+    print("The factorial of", num, "is", factorial)
